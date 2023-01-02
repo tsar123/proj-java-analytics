404a20264a7797e9ae2a96e4db58c77c10ebab0a
tsar123 <65504823+tsar123@users.noreply.github.com>
2023-01-02 19:37:23 +0500

    add taskRun and move to "task"

---
+++ src/main/java/task/main.java
@@ -0,0 +1,27 @@
+package task;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class main {
+    public static void main(String args[]) throws IOException {
+        boolean fullDatabase = false;
+        //если таблица заполнена данными, то fullDatabase = false
+        try {
+            var taskRun = new taskRun();
+            var dataLoad = databaseLoad.getInstance();
+            if(fullDatabase == true){
+                ArrayList<dataCountryIndicators> data = parser.getParse();
+                dataLoad.database(data);
+            }
+            taskRun.createBarChart(dataLoad);
+            taskRun.printHighEconomy(dataLoad);
+            taskRun.printIndicators(dataLoad);
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
