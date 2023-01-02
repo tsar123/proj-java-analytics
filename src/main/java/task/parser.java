addbf2588ca79bd717cb5337cd3baedd3f1d0e28
tsar123 <65504823+tsar123@users.noreply.github.com>
2023-01-02 19:33:44 +0500

    move to "task"

--- src/main/java/task/parser.java
+++ src/main/java/task/parser.java
@@ -1,8 +1,10 @@
-import java.io.IOException;
+package task;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.IOException;
 
 
 //Распарсив данные в файле CSV, нужно по ним создать набор объектов, заполнив все необходимые поля.
@@ -11,13 +13,13 @@ public class parser {
         ArrayList dataArr = new ArrayList<dataCountryIndicators>();
 
         try {
-            List<String> fileName = Files.readAllLines(Paths.get("dataset.csv"));
+            List<String> fileName = Files.readAllLines(Paths.get("D:\\Users\\tsarg\\CountryIndicators\\dataset.csv"));
             fileName.remove(0);
             fileName.forEach(dataString -> dataArr.add(new dataCountryIndicators(dataString.split(","))));
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        System.out.println("парсер работает");
         return dataArr;
     }
 }
