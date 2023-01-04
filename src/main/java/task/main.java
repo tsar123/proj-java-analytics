700ad0c7f7e5abf33d0185a4d2c7b58f9c2ebca9
tsar123 <65504823+tsar123@users.noreply.github.com>
2023-01-04 17:54:43 +0500

    Update main.java

--- src/main/java/task/main.java
+++ src/main/java/task/main.java
@@ -5,7 +5,7 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class main {
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         boolean fullDatabase = false //если таблица заполнена данными, то fullDatabase = false
         try {
             var taskRun = new taskRun();
