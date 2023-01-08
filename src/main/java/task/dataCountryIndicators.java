b00cbdca9147ec3c9a5876c0a839d0170c9a61a2
Tsaregorodtsev <65504823+tsar123@users.noreply.github.com>
2023-01-08 23:00:33 +0500

    Update dataCountryIndicators.java

--- src/main/java/task/dataCountryIndicators.java
+++ src/main/java/task/dataCountryIndicators.java
@@ -8,7 +8,7 @@ public class dataCountryIndicators {
     public final String region;
     public final int happyRank;
     public final float happyScore;
-    public final float standartError;
+    public final float standardError;
     public final float economy;
     public final float family;
     public final float health;
@@ -16,17 +16,17 @@ public class dataCountryIndicators {
     public final float trust;
     public final float generosity;
     public final float dystopiaResidual;
-    
+
     public float getEconomy() {
         return economy;
     }
 
-    dataCountryIndicators(String country, String region, int happyRank, float happyScore, float standartError, float economy, float family, float health, float freedom, float trust, float generosity, float dystopiaResidual) {
+    dataCountryIndicators(String country, String region, int happyRank, float happyScore, float standardError, float economy, float family, float health, float freedom, float trust, float generosity, float dystopiaResidual) {
         this.country = country;
         this.region = region;
         this.happyRank = happyRank;
         this.happyScore = happyScore;
-        this.standartError = standartError;
+        this.standardError = standardError;
         this.economy = economy;
         this.family = family;
         this.health = health;
@@ -41,7 +41,7 @@ public class dataCountryIndicators {
         this.region = str[1];
         this.happyRank = Integer.parseInt(str[2]);
         this.happyScore = Float.parseFloat(str[3]);
-        this.standartError = Float.parseFloat(str[4]);
+        this.standardError = Float.parseFloat(str[4]);
         this.economy = Float.parseFloat(str[5]);
         this.family = Float.parseFloat(str[6]);
         this.health = Float.parseFloat(str[7]);
@@ -54,6 +54,6 @@ public class dataCountryIndicators {
     @Override
     public String toString() {
         return String.format("Страна: %s | Регион: %s | Ранг(счастье): %d | Очки счастья: %.3f | Стандартная ошибка: %.5f | Экономика: %.5f | Семья: %.5f | Здоровье: %.5f | Свобода: %.5f | Доверие: %.5f | Щедрость: %.5f | Остаточная антиутопия: %.5f",
-                this.country, this.region, this.happyRank, this.happyScore, this.standartError, this.economy, this.family, this.health, this.freedom, this.trust, this.generosity, this.dystopiaResidual);
+                this.country, this.region, this.happyRank, this.happyScore, this.standardError, this.economy, this.family, this.health, this.freedom, this.trust, this.generosity, this.dystopiaResidual);
     }
 }
